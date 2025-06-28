2a67b458acc0f023d4045f40a523401f76627ba7
Harsh0it <harsh9548631129@gmail.com>
2025-06-28 10:56:04 +0530

    Initial commit - uploading full ML PROJECTS folder

---
+++ Simple Linear regression/package_predict.py
@@ -0,0 +1,26 @@
+import numpy as np
+import pandas as pd
+
+
+df=pd.read_csv( r'C:\Users\HP\Downloads\placement.csv')
+print(df.head())
+
+X=df.iloc[:,0:1]
+y=df.iloc[:,-1]
+
+from sklearn.model_selection import train_test_split
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=2)
+
+from sklearn.linear_model import LinearRegression
+
+lr=LinearRegression()
+
+lr.fit(X_train, y_train)
+
+cgpa_input = float(input("Enter your CGPA: "))
+
+# Predict package
+predicted_package = lr.predict([[cgpa_input]])  # 2D array
+
+print(f"Predicted Package: ₹{predicted_package[0]:.2f} LPA")
